87c5808bbcf711853686531917a698163e22345f
naveen <185893220+codamee@users.noreply.github.com>
2026-03-24 20:14:38 +0530

    style : add height in Welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,10 +5,10 @@ const Welcome = () => {
   return (
     <div className='sm:flex-row flex flex-col justify-between h-screen '>
       <div className='md:flex-1 lg:h-full'>
-        <img src="/kevin-matos-Nl_FMFpXo2g-unsplash.jpg" alt="bg" className='w-full max-[380px]:h-100 h-full  sm:h-full object-cover' />
+        <img src="/kevin-matos-Nl_FMFpXo2g-unsplash.jpg" alt="bg" className='w-full max-[380px]:h-95 h-full sm:h-full object-cover' />
       </div>
       <div className='flex flex-col justify-center items-center h-full bg-blue-50 lg:rw-125'>
-        <div className='lg:w-[60%] w-full gap-6 flex flex-col justify-center lg:bg-slate-200 p-5 rounded-xl shadow-md'>
+        <div className='lg:w-[60%] max-sm:h-full w-full gap-6 flex flex-col justify-center lg:bg-slate-200 p-5 rounded-xl shadow-md'>
           <div className='flex flex-col gap-4'>
             <h1 className='text-2xl font-bold'>Welcome to PopX</h1>
             <p className='text-mauve-500 text-[1.1rem] sm:w-full w-[75%]'> Join PopX today and take control of your digital presence.</p>
